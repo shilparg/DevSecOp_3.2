b8d06eb3ece7dd7d9c274f78bf0bc567e4b85d7e
shilparg <shilparg_2000@yahoo.com>
2025-11-16 23:43:57 +0800

    devsecops 3.2 new

--- main.tf
+++ main.tf
@@ -22,9 +22,8 @@ provider "aws" {
 data "aws_caller_identity" "current" {}
 
 locals {
-  raw_name     = split("/", data.aws_caller_identity.current.arn)[1]
-  name_prefix = lower(replace(replace(raw_name, "-", ""), ".", ""))
-  account_id   = data.aws_caller_identity.current.account_id
+  name_prefix = "${split("/", "${data.aws_caller_identity.current.arn}")[1]}"
+  account_id  = "${data.aws_caller_identity.current.account_id}"
 }
 
 resource "aws_s3_bucket" "s3_tf" {
