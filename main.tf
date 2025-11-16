e60023d8524b6fb384d8dcf0d8113595b0f64372
shilparg <shilparg_2000@yahoo.com>
2025-11-16 23:48:42 +0800

    devsecops 3.2 new

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 
   backend "s3" {
     bucket = "sctp-ce11-tfstate"
-    key    = "shilpa/s3-tfstate/terraform.tfstate"  #"shilpa-s3-tf-ci.tfstate" # Updated for clarity and structure
+    key    = "shilpa/s3-tfstate/terraform.tfstate"
     region = "us-east-1"
   }
 }
@@ -22,8 +22,8 @@ provider "aws" {
 data "aws_caller_identity" "current" {}
 
 locals {
-  name_prefix = "${split("/", "${data.aws_caller_identity.current.arn}")[1]}"
-  account_id  = "${data.aws_caller_identity.current.account_id}"
+  name_prefix = split("/", data.aws_caller_identity.current.arn)[1]
+  account_id  = data.aws_caller_identity.current.account_id
 }
 
 resource "aws_s3_bucket" "s3_tf" {
